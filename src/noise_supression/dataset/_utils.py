66f5f0d8775be0eeb1cf60cfe4d8b9f5f75a46ff
Timur Kasimov <timurcat7@gmail.com>
2021-02-02 19:34:40 +0700

    Added type hints. Code style fixes.

--- src/noise_supression/dataset/_utils.py
+++ src/noise_supression/dataset/_utils.py
@@ -1,9 +1,12 @@
+import numbers
+from typing import Optional, Dict, Tuple
+
 import librosa
 import numpy as np
 
 
-def read_audio(path, sample_rate, normalize=True):
-    audio, sr = librosa.load(path, sample_rate)
+def read_audio(path: str, sample_rate: Optional[int], normalize=True, **kwargs: Dict) -> Tuple[np.ndarray, int]:
+    audio, sr = librosa.load(path, sample_rate, **kwargs)
 
     if normalize:
         librosa.util.normalize(audio)
@@ -11,40 +14,14 @@ def read_audio(path, sample_rate, normalize=True):
     return audio, sr
 
 
-def add_noise(clean_audio, noise):
+def add_noise(clean_audio: np.ndarray, noise: np.ndarray) -> np.ndarray:
     while clean_audio.size >= noise.size:
         noise = np.append(noise, noise)
 
-    i = np.random.randint(0, noise.size - clean_audio.size)
-    noise_segment = noise[i:i + clean_audio.size]
+    i: int = np.random.randint(0, noise.size - clean_audio.size)
+    noise_segment: np.ndarray = noise[i:i + clean_audio.size]
 
-    clean_power = np.sum(np.square(clean_audio))
-    noise_power = np.sum(np.square(noise_segment))
+    clean_power: numbers.Real = np.sum(np.square(clean_audio))
+    noise_power: numbers.Real = np.sum(np.square(noise_segment))
 
     return clean_audio + np.sqrt(clean_power / noise_power) * noise_segment
-
-# def _signal_rms(signal: np.ndarray) -> float:
-#     return np.sqrt(np.sqrt(np.mean(np.sqr(signal))))
-#
-#
-# def _noise_rms(sig_rms: float, target_snr: float = 20) -> float:
-#     return np.sqrt(sig_rms * sig_rms / np.power(10, target_snr / 10))
-#
-#
-# def add_awgn(signal: np.ndarray, target_snr: float = 20) -> np.ndarray:
-#     sig_rms = _signal_rms(signal)
-#     noise_rms = _noise_rms(sig_rms, target_snr)
-#
-#     noise = np.random.normal(0, noise_rms, signal.shape[0])
-#
-#     return signal + noise
-#
-#
-# def add_real_world_noise(signal: np.ndarray, noise: np.ndarray, target_snr: float = 20) -> np.ndarray:
-#     sig_rms = _signal_rms(signal)
-#     cur_noise_rms = _signal_rms(noise)
-#     target_noise_rms = _noise_rms(sig_rms, target_snr)
-#
-#     coef = target_noise_rms / cur_noise_rms
-#
-#     return signal + coef * noise
--- src/noise_supression/dataset/dataset.py
+++ src/noise_supression/dataset/dataset.py
@@ -1,20 +1,23 @@
 import multiprocessing as mp
 import warnings
 from pathlib import Path
-from typing import List
+from typing import List, Dict
 
 import numpy as np
 import soundfile as sf
 
+from src.noise_supression.dataset._const import NP_RANDOM_SEED
 from src.noise_supression.dataset._utils import add_noise, read_audio
 
 warnings.filterwarnings('ignore', category=UserWarning)
 
 _CORES: int = mp.cpu_count()
 
+np.random.seed(NP_RANDOM_SEED)
+
 
 class Dataset:
-    def __init__(self, out_path: Path, **kwargs) -> None:
+    def __init__(self, out_path: Path, **kwargs: Dict) -> None:
         self.__out_path: Path = out_path
         self.__sample_rate: int = kwargs['sample_rate']
         self.__overlap: int = kwargs['overlap']
@@ -29,12 +32,11 @@ class Dataset:
                 break
 
             clean_audio, _ = read_audio(clean_file, self.__sample_rate)
-            noised = add_noise(clean_audio, noise)
+            noised: np.ndarray = add_noise(clean_audio, noise)
 
             sf.write(self.__out_path / Path(clean_file).name, noised, self.__sample_rate, format='WAV')
 
     def create(self, clean_filenames: List[str], noise_filenames: List[str]) -> None:
-        print('Creating arguments list for Pool...')
         samples = ((clean_file, read_audio(np.random.choice(noise_filenames), self.__sample_rate)[0]) for clean_file in
                    clean_filenames)
 
@@ -45,7 +47,6 @@ class Dataset:
         for samp in samples:
             q.put(samp)
 
-        # tell workers we're done
         for _ in range(_CORES):
             q.put(None)
 
