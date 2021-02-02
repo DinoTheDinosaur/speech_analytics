8a2ffa74001e4fb8236cd8515f91da6a0f8f521c
Nikolay Sheregeda <ns.sheregeda@gmail.com>
2021-02-02 10:15:25 +0300

    Add voice activity detection module

---
+++ src/vad.py
@@ -0,0 +1,110 @@
+import os
+import wave
+import tempfile
+
+import torch
+
+from pyannote.audio.utils.signal import Binarize
+from scipy.io import wavfile
+
+
+class VADException(Exception):
+    pass
+
+
+class VoiceActivityDetection:
+
+    def __init__(self, binarize_params=None):
+        self.sad = torch.hub.load('pyannote/pyannote-audio', model='sad_ami')
+        # см. VAD Smoothing в статье https://www.isca-speech.org/archive/
+        # interspeech_2015/papers/i15_2650.pdf
+        binarize_params_default = {
+            # an onset and offset thresholds for the detection of
+            # the beginning and end of a speech segment
+            'offset': 0.5,
+            'onset': 0.5,
+            # a threshold for small silence deletion
+            'min_duration_off': 0.1,
+            # a threshold for short speech segment deletion;
+            'min_duration_on': 0.1,
+            'log_scale': True,
+        }
+        binarize_params = binarize_params or binarize_params_default
+        self.binarize = Binarize(**binarize_params)
+
+    @staticmethod
+    def validate_wav_file(file_path):
+        try:
+            with wave.open(file_path, 'rb') as f:
+                if f.getnchannels() != 2:
+                    raise VADException(
+                        'Invalid number of channels for wav file'
+                    )
+        except wave.Error as e:
+            raise VADException(f'Invalid format of wav file: {e}')
+
+    @staticmethod
+    def prepare_wav_by_channels(
+            source_wav, operator_channel, client_channel, tmpdir
+    ):
+        rate, data = wavfile.read(source_wav)
+        operator_data = data[:, operator_channel]
+        client_data = data[:, client_channel]
+
+        operator_file_path = os.path.join(tmpdir, 'operator.wav')
+        client_file_path = os.path.join(tmpdir, 'client.wav')
+
+        wavfile.write(operator_file_path, rate, operator_data)
+        wavfile.write(client_file_path, rate, client_data)
+
+        return operator_file_path, client_file_path
+
+    def get_timeline(self, file_path):
+        sad_scores = self.sad({'uri': 'filename', 'audio': file_path})
+        speech = self.binarize.apply(sad_scores, dimension=0)
+        return speech.for_json()['content']
+
+    def get_timelines(self, file_path, operator_channel):
+        """
+        Для двухканального wav-файла возвращает разметку/таймлайн разговора.
+
+        :note:
+            Предполагается, что оператор и клиент разнесены по двум разным
+            каналам.
+
+        :param file_path:
+            `str`, путь до исходного wav-файла.
+        :param operator_channel:
+            `int`, номер канала, который относится к оператору.
+
+        :return:
+            `dict`, словарь разметки вида:
+            {
+                'operator_timeline': [
+                    {'start': 10, 'end': '12'},
+                    ...
+                ],
+                'client_timeline': [
+                    {'start': 13, 'end': '20'},
+                    ...
+                ]
+            }
+        """
+        if operator_channel not in (0, 1):
+            raise VADException('Invalid number of operator channel')
+
+        client_channel = 0 if operator_channel else 1
+
+        self.validate_wav_file(file_path)
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            operator_wav, client_wav = self.prepare_wav_by_channels(
+                file_path, operator_channel, client_channel, tmpdir
+            )
+            operator_timeline = self.get_timeline(operator_wav)
+            client_timeline = self.get_timeline(client_wav)
+
+        return {
+            'operator_timeline': operator_timeline,
+            'client_timeline': client_timeline,
+        }
