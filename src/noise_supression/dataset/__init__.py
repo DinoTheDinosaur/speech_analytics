d8b76f4156b5a634db865449dfbebc2017398f4d
Timur Kasimov <timurcat7@gmail.com>
2021-02-02 13:49:02 +0700

    Added __all__.

---
+++ src/noise_supression/dataset/__init__.py
@@ -0,0 +1,5 @@
+from src.noise_supression.dataset.mcv import MCV
+from src.noise_supression.dataset.urban_sound_8k import UrbanSound8k
+from src.noise_supression.dataset.dataset import Dataset
+
+__all__ = ['MCV', 'UrbanSound8k', 'Dataset']
