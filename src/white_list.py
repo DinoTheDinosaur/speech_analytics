28a4cc68b6526f09667392dc8cfee31a8b13d6d0
PredelinaAsya <54816157+PredelinaAsya@users.noreply.github.com>
2021-02-04 14:59:44 +0500

    Changed library for lemmatization and fixed counting phrases from white list.

--- src/white_list.py
+++ src/white_list.py
@@ -1,43 +1,20 @@
 import re
-import spacy
+from pymorphy2 import MorphAnalyzer
 import json
 from nltk.tokenize import word_tokenize
 
 
 def search_phrase(input_text: str, phrase: str) -> bool:
     input_text = re.sub(r'[^\w\s]', '', input_text)
-    input_text.lower()
     phrase = re.sub(r'[^\w\s]', '', phrase)
-    phrase.lower()
 
-    nlp = spacy.load('ru2')
-    input_words = [word.lemma_ for word in nlp(input_text)]
-    phrase_words = [word.lemma_ for word in nlp(phrase)]
+    morph = MorphAnalyzer()
+    input_words = [morph.parse(word)[0].normal_form for word in input_text.split()]
+    phrase_words = [morph.parse(word)[0].normal_form for word in phrase.split()]
     input_text = ' '.join(input_words)
     phrase = ' '.join(phrase_words)
 
     return input_text.find(phrase) != -1
-
-
-def search_one_syn_phrase(input_text: str, syn_phrases: list) -> bool:
-    """
-    :param input_text: input text of operator's speech
-    :param syn_phrases: list of dictionaries with keys: 'phrase' which means one of synonymous phrases, type of value
-    is str; and 'keywords' which means str with all keywords in phrase, type of value is str;
-    """
-    output = False
-    for dct in syn_phrases:
-        if dct['keywords'] == 'all':
-            output |= search_phrase(input_text, dct['phrase'])
-            continue
-
-        words = word_tokenize(dct['keywords'], language="russian")
-        all_keywords_in_text = True
-        for word in words:
-            all_keywords_in_text &= search_phrase(input_text, word)
-        output |= all_keywords_in_text
-
-    return output
 
 
 class WhiteCheck:
@@ -47,6 +24,27 @@ class WhiteCheck:
         """
         with open(file_with_list, "r", encoding='utf-8') as file:
             self.list = json.load(file)
+
+    @staticmethod
+    def search_one_syn_phrase(input_text: str, syn_phrases: list) -> bool:
+        """
+        :param input_text: input text of operator's speech
+        :param syn_phrases: list of dictionaries with keys: 'phrase' which means one of synonymous phrases, type of value
+        is str; and 'keywords' which means str with all keywords in phrase, type of value is str;
+        """
+        output = False
+        for dct in syn_phrases:
+            if dct['keywords'] == 'all':
+                output |= search_phrase(input_text, dct['phrase'])
+                continue
+
+            words = word_tokenize(dct['keywords'], language="russian")
+            all_keywords_in_text = True
+            for word in words:
+                all_keywords_in_text &= search_phrase(input_text, word)
+            output |= all_keywords_in_text
+
+        return output
 
     def update_list(self, arr_of_syn_sent: list, file_with_list: str):
         """
@@ -58,9 +56,9 @@ class WhiteCheck:
         with open(file_with_list, "w", encoding='utf-8') as file:
             json.dump(self.list, file, ensure_ascii=False)
 
-    def search_all_phrases(self, input_text: str) -> bool:
-        output = True
+    def count_white_phrases(self, input_text: str) -> list:
+        out_list = []
         for arr in self.list:
-            output &= search_one_syn_phrase(input_text, arr)
+            out_list.append(int(self.search_one_syn_phrase(input_text, arr)))
 
-        return output
+        return out_list
