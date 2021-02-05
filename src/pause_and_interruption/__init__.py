052476ce1b37cf02ce60ee6b2bbc4e51bcf33092
Timur Kasimov <timurcat7@gmail.com>
2021-02-05 22:59:45 +0700

    Init commit.

---
+++ src/pause_and_interruption/__init__.py
@@ -0,0 +1,4 @@
+from src.pause_and_interruption.pause import pause_detection
+from src.pause_and_interruption.interruption import interruption_detection
+
+__all__ = ['pause_detection', 'interruption_detection']
