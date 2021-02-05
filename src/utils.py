6a504f2887d719a015e762b58d104b731ac0c5db
Timur Kasimov <timurcat7@gmail.com>
2021-02-05 17:38:38 +0700

    Added function for clearing folder.

--- src/utils.py
+++ src/utils.py
@@ -3,3 +3,11 @@ from pathlib import Path
 
 def validate_dir(path: Path):
     return path.exists() and path.is_dir()
+
+
+def clear_dir(path: Path):
+    for child in path.iterdir():
+        if child.is_file():
+            child.unlink(missing_ok=True)
+        else:
+            clear_dir(child)
